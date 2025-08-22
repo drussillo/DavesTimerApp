4b5eb06741d6deb7092d1b66ff64a494fb96d6bb
drussillo <daverussillo7@gmail.com>
2025-08-22 15:15:14 -0500

    Added timer ending detection system

--- timer.sh
+++ timer.sh
@@ -3,5 +3,20 @@
 # simple timer app
 #
 #
-
 CURRENT_TIME="$(date +%s)"
+TIMERS="$(ls ./current_timers/)"
+
+while [[ -n $TIMERS ]]; do
+  CURRENT=$(echo "$TIMERS" | awk 'NR==1')
+
+  # one of the timers < current time
+  if [[ $CURRENT < $(date +%s) ]]; then
+    # delete timer & alert user
+    rm ./current_timers/$CURRENT
+    ffplay -nodisp -autoexit ./alert_sounds/city_alert_siren.wav
+    # INSERT USER SPECIFIED EFFECTS HERE
+  fi
+
+  TIMERS=$(echo "$TIMERS" | tail -n +2)
+done
+
