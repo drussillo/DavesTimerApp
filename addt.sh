9514f225f656602eb161e73a6d5c0dbfd9899669
drussillo <daverussillo7@gmail.com>
2025-08-22 22:38:13 -0500

    -Added remove timer feature - Simplified timer.sh (no idea what I was doing) - Fixed other stuff

--- addt.sh
+++ addt.sh
@@ -3,16 +3,16 @@
 #
 #
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
-echo -e "\nDavesTimerApp: command-line-interface\n"
-echo "(* means optional)"
+echo "-------------------------------------"
+echo "DavesTimerApp: command-line-interface"
 echo "-------------------------------------"
 
 
-echo -en "Timer (1) or Alarm (2) \n..? "
+echo -en "Timer (1), Alarm (2), Remove (3) \n..? "
 read -r CHOICE
 
 if [[ $CHOICE == 1 ]]; then
-  echo -en "Enter time (h*:m) \n..? "
+  echo -en "\nEnter time (h:m) \n..? "
   read -r TIMER
 
   MINUTES="$(echo "$TIMER" | awk -F: '{print $2}')"
@@ -28,8 +28,56 @@ if [[ $CHOICE == 1 ]]; then
 
   touch "$SCRIPT_PATH/current_timers/$TIMER"
 
+  echo "New timer: $(date -d "@$TIMER" +%m/%d\ -\ %H:%M:%S) - $((($TIMER - $(date +%s)) / 60))min remaining"
+  exit
+
 elif [[ $CHOICE == 2 ]]; then
-  echo "2"
+
+  #TODO
+  echo "message"
+  exit
+
+elif [[ $CHOICE == 3 ]]; then
+
+  echo
+  TIMERS="$(ls $SCRIPT_PATH/current_timers/)"
+  if [[ -n $TIMERS ]]; then
+    echo -e "\nCurrent Timers:"
+
+    # Display all timers in right format
+
+    TEMP=$(echo "$TIMERS" | awk 'NR==1')
+    echo "$TEMP"
+    i=1
+    while [[ -n $TEMP ]]; do
+      echo "($i)  $(date -d "@$TEMP" +%m/%d\ -\ %H:%M:%S) - $((($TEMP - $(date +%s)) / 60))min remaining"
+      i=$(($i+1))
+      TEMP=$(echo "$TIMERS" | awk -v line="$i" 'NR==line')
+    done
+    echo -e "($i)  All\n"
+
+    echo -en "Pick an option to delete \n..? "
+    read -r CHOICE
+    echo
+
+    # all option
+    if [[ $CHOICE == $i ]]; then
+      rm $SCRIPT_PATH/current_timers/1*
+      echo "All timers have been cleared"
+      exit
+    elif [[ $CHOICE > $i || $CHOICE < 1 ]]; then
+      echo "Invalid timer entry" >&2
+    else
+      rm $SCRIPT_PATH/current_timers/$(echo "$TIMERS" | awk -v line="$CHOICE" 'NR==line')
+      echo "Timer ($CHOICE) has been cleared"
+      exit
+    fi
+
+  else
+    echo "No timers currently active."
+    exit
+  fi
+
 else
   echo "Invalid choice!" >&2
   exit
--- timer.sh
+++ timer.sh
@@ -1,38 +1,26 @@
 #!/usr/bin/bash
 #
-# simple timer app
 #
 #
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
-TIMERS="$(ls $SCRIPT_PATH/current_timers/)"
+NEXT_TIMER="$(ls -X $SCRIPT_PATH/current_timers/ | awk 'NR==1')"
 
-if [[ -n $TIMERS ]]; then
+if [[ -n "$NEXT_TIMER" ]]; then
   CURRENT_TIME="$(date +%s)"
 
-  # next timer (to display)
-  NEXT=$(echo "$TIMERS" | awk 'NR==1')
-
-  # go through all timer files
-  while [[ -n $TIMERS ]]; do
-    CURRENT=$NEXT
-
-    # one of the timers < current time
-    if [[ $CURRENT < $(date +%s) ]]; then
-      # delete timer & alert user
-      rm $SCRIPT_PATH/current_timers/$CURRENT
-      ffplay -nodisp -autoexit $SCRIPT_PATH/alert_sounds/city_alert_siren.wav
-      # INSERT USER SPECIFIED EFFECTS HERE
-    fi
-
-    TIMERS=$(echo "$TIMERS" | tail -n +2)
-  done
+  if [[ $NEXT_TIMER -lt $CURRENT_TIME ]]; then
+    # delete timer & alert user
+    rm $SCRIPT_PATH/current_timers/NEXT_TIMER$
+    # INSERT USER SPECIFIED EFFECTS HERE
+    ffplay -nodisp -autoexit $SCRIPT_PATH/alert_sounds/city_alert_siren.wav
+  fi
 
   # for i3blocks
-  DISPLAY="$(date -d "@$NEXT" +%m/%d\ -\ %H:%M) - "
+  DISPLAY="$(date -d "@$NEXT_TIMER" +%m/%d\ -\ %H:%M) - "
 
   # make sure time left is diplayed in 00:00 instead of 0:0
-  MINUTES=$((($NEXT-$CURRENT_TIME) / 60))
-  SECONDS=$((($NEXT-$CURRENT_TIME) % 60))
+  MINUTES=$((($NEXT_TIMER-$CURRENT_TIME) / 60))
+  SECONDS=$((($NEXT_TIMER-$CURRENT_TIME) % 60))
 
   if [[ $MINUTES -lt 10 ]]; then
     DISPLAY+="0$MINUTES:"
@@ -44,7 +32,6 @@ if [[ -n $TIMERS ]]; then
   else
     DISPLAY+="$SECONDS"
   fi
-
 
   echo "$DISPLAY"
   echo "$DISPLAY"
